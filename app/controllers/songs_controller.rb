7c9b90127beae08b72d00de8bf640e53e08f7473
justin_james <jjames11799@gmail.com>
2020-06-25 17:36:51 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -9,11 +9,14 @@ class SongsController < ApplicationController
 
   def new
     @song = Song.new
+    @genres = Genre.all 
+    # 2.times {@song.notes.build }
   end
 
   def create
+    # byebug
     @song = Song.new(song_params)
-
+    
     if @song.save
       redirect_to @song
     else
@@ -47,7 +50,7 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(:title, :artist_name, :genre_id, note_contents: [])
   end
 end
 
